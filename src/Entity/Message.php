a3ebfab216d82c202cb2c6f404c501502bda4141
af1nasii <aleksejabmetko@gmail.com>
2025-04-02 02:41:24 -0500

    add forum controller

---
+++ src/Entity/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'text')]
+    private string $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+}
